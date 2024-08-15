68a54479eb19e9111b80534730e016054b21193b
aartitalekar734 <aartitalekar734@gmail.com>
2024-08-15 22:20:51 +0530

    string implicit conversion
    string implicit conversion

---
+++ 05-implicit-conversation.js
@@ -0,0 +1,23 @@
+console.log("Implicit conversation");
+
+var result;
+
+console.log("concatenation of string and number is");
+
+result = "3" + 2;
+console.log(result);
+console.log("====================================");
+console.log("concatenation of string and Boolean is");
+
+result = "3" + true;
+console.log(result);
+console.log("=========================================");
+console.log("concatenation of string and undefined is");
+
+result = "3" + undefined;
+console.log(result);
+console.log("=======================================");
+console.log("concatenation of string and null is");
+
+result = "3" + null;
+console.log(result);
